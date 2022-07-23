a27d84bac12aa15486f4ed0d500331e81e9c6c49
Jay Vadhavana <jayvadhavana23@gmail.com>
2022-07-23 11:28:54 +0530

    Added home page and Editor page

---
+++ project_frontend/src/pages/EditorPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function EditorPage() {
+  return <div>Editor_page</div>;
+}
+
+export default EditorPage;
---
+++ project_frontend/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="homePageWrapper d-flex justify-content-center align-items-center">
+      <div className="formWrapper w-25">Form</div>
+    </div>
+  );
+}
+
+export default Home;
