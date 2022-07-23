3858016107862db0dc74765a0d8cd589eda6822a
Jay Vadhavana <jayvadhavana23@gmail.com>
2022-07-23 22:00:32 +0530

    Form added for create room and join room
    - Added toast
    - Handle validation
    - copy text

--- project_frontend/src/components/Form.js
+++ project_frontend/src/components/Form.js
@@ -1,54 +1,135 @@
-import React from "react";
+import React, { useState } from "react";
 import { Tabs, Tab } from "react-bootstrap";
+import { v4 as uuidv4 } from "uuid";
+import { Toast } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 function Form() {
-  return (
-    <div className="form-container rounded-1">
-      <Tabs
-        justify
-        variant="pills"
-        defaultActiveKey="tab-1"
-        className="tab-container mb-1 p-0 border-bottom "
-      >
-        <Tab eventKey="tab-1" title="Create Room">
-          <div className="input-group p-2">
-            <input type="text" class="form-control" />
-            <span
-              class="input-group-text"
-              style={{ cursor: "pointer" }}
-              id="copyBtn"
-            >
-              <i class="bi bi-files"></i>
-            </span>
-          </div>
-        </Tab>
-        <Tab eventKey="tab-2" title="Join Room">
-          <form className="p-2">
-            <div className="mb-3">
-              <label for="name" className="form-label">
-                Name
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="name"
-                aria-describedby="emailHelp"
-              />
-            </div>
-            <div className="mb-3">
-              <label for="roomId" className="form-label">
-                Room Id
-              </label>
-              <input type="text" className="form-control" id="roomId" />
-            </div>
+  const navigate = useNavigate();
 
-            <button type="submit" className="btn button">
-              Submit
-            </button>
-          </form>
-        </Tab>
-      </Tabs>
-    </div>
+  const [roomId, setRoomId] = useState("");
+  const [userName, setUserName] = useState("");
+  const [roomIdJoin, setRoomIdJoin] = useState("");
+  const [showToast, setToast] = useState(false);
+  const [toastHeader, setToastHeader] = useState("");
+  const [toastBody, setToastBody] = useState("");
+  const [toastColor, setToastColor] = useState("");
+
+  const createNewRoom = (e) => {
+    const id = uuidv4();
+    setRoomId(id);
+    console.log(id);
+  };
+
+  const copyText = () => {
+    navigator.clipboard.writeText(roomId);
+    setToastHeader("Copied!");
+    setToastBody("Text is copied to clipboard.");
+    setToastColor("green");
+    setToast(true);
+  };
+
+  const joinRoom = (e) => {
+    e.preventDefault();
+    if (!roomIdJoin || !userName) {
+      setToastHeader("Warning!");
+      setToastBody("Room ID and Name is required.");
+      setToastColor("red");
+      setToast(true);
+      return;
+    }
+
+    navigate(`/editor/${roomIdJoin}`, {
+      state: {
+        userName,
+      },
+    });
+  };
+
+  return (
+    <>
+      <div className="form-container rounded-1 ">
+        <Tabs
+          justify
+          variant="pills"
+          defaultActiveKey="tab-1"
+          className="tab-container mb-1 p-0 border-bottom"
+        >
+          <Tab eventKey="tab-1" title="Create Room">
+            <div className="p-2">
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  onChange={(e) => setRoomId(e.target.value)}
+                  value={roomId}
+                  readOnly
+                />
+                <span
+                  className="input-group-text"
+                  style={{ cursor: "pointer" }}
+                  id="copyBtn"
+                  onClick={copyText}
+                >
+                  <i className="bi bi-files"></i>
+                </span>
+              </div>
+              <button className="btn button mt-2" onClick={createNewRoom}>
+                Generate ID
+              </button>
+            </div>
+          </Tab>
+          <Tab eventKey="tab-2" title="Join Room">
+            <form className="p-2">
+              <div className="mb-3">
+                <label htmlFor="name" className="form-label">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  onChange={(e) => setUserName(e.target.value)}
+                  value={userName}
+                  aria-describedby="emailHelp"
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="roomIdJoin" className="form-label">
+                  Room Id
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="roomIdJoin"
+                  onChange={(e) => setRoomIdJoin(e.target.value)}
+                  value={roomIdJoin}
+                />
+              </div>
+
+              <button type="submit" className="btn button" onClick={joinRoom}>
+                Join
+              </button>
+            </form>
+          </Tab>
+        </Tabs>
+      </div>
+
+      <div className="p-2">
+        <Toast
+          onClose={() => setToast(false)}
+          autohide
+          show={showToast}
+          delay={2200}
+          style={{ background: toastColor, color: "#ffffff" }}
+        >
+          <Toast.Header>
+            <strong className="mr-auto">{toastHeader}</strong>
+          </Toast.Header>
+          <Toast.Body>{toastBody}</Toast.Body>
+        </Toast>
+      </div>
+    </>
   );
 }
 
--- project_frontend/src/pages/Home.js
+++ project_frontend/src/pages/Home.js
@@ -4,7 +4,7 @@ import Form from "../components/Form";
 function Home() {
   return (
     <div className="container-fluid min-vw-100 min-vh-100 homePageWrapper d-flex align-items-center justify-content-center">
-      <div className="border border-1 rounded-1 w-25">
+      <div className="border border-1 rounded-1 w-25 shadow">
         <Form />
       </div>
     </div>
