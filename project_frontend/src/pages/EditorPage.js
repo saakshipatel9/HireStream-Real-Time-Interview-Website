974c3b12065455df9f3a2fe0639b2f9ecfc6c177
Jay Vadhavana <jayvadhavana23@gmail.com>
2022-08-01 12:35:45 +0530

    Logo added

--- project_frontend/src/pages/EditorPage.js
+++ project_frontend/src/pages/EditorPage.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Sidebar from "../components/Sidebar";
-import EditorComponent from "../components/EditorComponent";
+// import EditorComponent from "../components/EditorComponent";
 
 function EditorPage() {
   return (
     <div className="mainWrap container-fluid mh-100 overflow-hidden">
       <div className="row">
         <div className="editor-container col-lg-9 border-3 border-end">
-          <EditorComponent />
+          {/* <EditorComponent /> */}
         </div>
         <div className="aside min-vh-100 col-lg-3">
           <Sidebar />
--- project_frontend/src/pages/Home.js
+++ project_frontend/src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Form from "../components/Form";
+import Logo from "../image-removebg-preview.png";
 
 function Home() {
   return (
-    <div className="container-fluid min-vw-100 min-vh-100 homePageWrapper d-flex align-items-center justify-content-center">
+    <div className="container-fluid min-vw-100 homePageWrapper d-flex flex-column align-items-center justify-content-center">
+      <div className="logo">
+        <img src={Logo} alt="" style={{ width: "754px" }} />
+      </div>
       <div className="border border-1 rounded-1 w-25 shadow">
         <Form />
       </div>
