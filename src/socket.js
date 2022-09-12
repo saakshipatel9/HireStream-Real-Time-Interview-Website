85584ba22e3714272badc2c0144b86aee187472b
Jay Vadhavana <jayvadhavana23@gmail.com>
2022-09-12 17:22:42 +0530

    changed to dynamic port

--- src/socket.js
+++ src/socket.js
@@ -8,5 +8,5 @@ export const initSocket = async () => {
     transports: ["websocket"],
   };
 
-  return io("http://localhost:5000", options);
+  return io(process.env.PORT || "http://localhost:5000", options);
 };
